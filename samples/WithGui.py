647cfd8e7fb697b30753d76917a39411966ba5bd
sktrain <39360116+sktrain@users.noreply.github.com>
2026-02-08 19:18:35 +0100

    Add files via upload

---
+++ samples/WithGui.py
@@ -0,0 +1,8 @@
+# Source - https://stackoverflow.com/a
+# Posted by Ryan Ginstrom
+# Retrieved 2025-11-16, License - CC BY-SA 2.5
+
+import easygui
+
+easygui.msgbox("This is a message!", title="simple gui")
+easygui.enterbox("Bitte Zahl eingeben:")
