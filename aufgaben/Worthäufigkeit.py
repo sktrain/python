436e03a9db4cd3028a6246135c90fe6d1a438f6e
sktrain <39360116+sktrain@users.noreply.github.com>
2026-03-10 07:55:06 +0100

    Create Worthäufigkeit.py

---
+++ aufgaben/Worthäufigkeit.py
@@ -0,0 +1,6 @@
+# Worthäufigkeit bestimmen:
+# Wie können wir die Häufigkeit von Worten in einer gegebenen Liste bestimmen und speichern
+
+strings = ["Baby Shark", "Corona", "Baby Yoda", "Corona", "Baby Yoda", "Tiger King",
+    "David Bowie", "Kylie Jenner", "Kardashian", "Love Island", "Bachelorette",
+    "Baby Yoda", "Tiger King", "Billie Eilish", "Corona"]
